d847a3ce6e158d4abd958b6750898e986714107d
Tina Kamali-Anaraki <tina.kamali15@gmail.com>
2022-05-23 23:22:12 +0000

    added ec2 folder in  devops folder

---
+++ devops/ec2/provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+
+  access_key = "ASIA22VGYN2GWEAYUFFB"
+  secret_key = "Cm8MuF2oUwtILIr0M35DV+q9LXJmDAzGpUgrLNvG"
+
+  token = "FwoGZXIvYXdzEJH//////////wEaDIaCzyj08IqH5eNBJiK4AVKenn9uaIqonE7YNB0BuBT48oVCwY+d5MYA9AfjAoZZHSPk7PvlyJUpYj7sEpIgWSf7XIo/Gj3ukM9C/Qx4xPeryy1dbEp8pLhK9uptUvSm2z9cGuFjpUFuyRDmazdGVX8zfBD7AQtvYlQqxLFlMNuph2r49aZDdFeUPao7jsyNCEmvkyeUoXcLsVQSG4NNypUtq6OHDO7vspaWyjJoh9QrZgbIvfM9/HFTxrsLAEjezhFFPHozckQorKqwlAYyLf4KY1zTRG9ndpTo468cB5mQDSfiNPDNgpCC2lPeB4NopTF/wZndn3ewpZ4Kbw=="
+
+  region = var.aws_region
+
+}
+
+
