d3041d6411bd165e23f6d4722e193ad9b84d8b9c
Tina Kamali-Anaraki <tina.kamali15@gmail.com>
2022-05-24 14:12:54 +0000

    modifying access key, token and secret key, because simplilearn aws session expires every ~ 8 hours and generate new keys

--- devops/ec2/provider.tf
+++ devops/ec2/provider.tf
@@ -1,11 +1,9 @@
 provider "aws" {
 
-  access_key = "ASIA22VGYN2GWEAYUFFB"
-  secret_key = "Cm8MuF2oUwtILIr0M35DV+q9LXJmDAzGpUgrLNvG"
+  access_key = "ASIA22VGYN2GZKCK42HM"
+  secret_key = "Z0c1IYsWgI+nIDgomMlJL8nIP1uGLX0FOLgtMWAK"
 
-  token = "FwoGZXIvYXdzEJH//////////wEaDIaCzyj08IqH5eNBJiK4AVKenn9uaIqonE7YNB0BuBT48oVCwY+d5MYA9AfjAoZZHSPk7PvlyJUpYj7sEpIgWSf7XIo/Gj3ukM9C/Qx4xPeryy1dbEp8pLhK9uptUvSm2z9cGuFjpUFuyRDmazdGVX8zfBD7AQtvYlQqxLFlMNuph2r49aZDdFeUPao7jsyNCEmvkyeUoXcLsVQSG4NNypUtq6OHDO7vspaWyjJoh9QrZgbIvfM9/HFTxrsLAEjezhFFPHozckQorKqwlAYyLf4KY1zTRG9ndpTo468cB5mQDSfiNPDNgpCC2lPeB4NopTF/wZndn3ewpZ4Kbw=="
-
-  region = var.aws_region
+  token = "FwoGZXIvYXdzEJ///////////wEaDBfe0UrHm0WO+0IJjyK4Aat76JjPDNlUfgTvjlNwEK5O4KkQJSsUOt2JYmfF2zWR/x2GRZz3KLNzgfBqVo6Uy3Ga7ZraGVij9FX+kyGf8Q44IQauqZ/oh75XjiTXGJl9AXKHHi/7d2TAerOW9lAnUgoo2aroDyk4dyMCSGhCcDOxfXQJkkOAjnW7qHP40yvZOW6w4JCfkB2kEZk1mUjK0GTUP+IsOUklWCqHujnrGgaRkn+mO18ZKF395LBhrZNeRjVVuMMYpOAo0b6zlAYyLSjEF0j1dM6u1eTS4u7MXE7Md+NXbClQeyqPanitgvAxe67/r4bc9KQXAtsOdQ=="
 
 }
 
