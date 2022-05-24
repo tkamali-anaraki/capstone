e13d9d3989771ff72c1d46b6065edecf558aab2d
Tina Kamali-Anaraki <tina.kamali15@gmail.com>
2022-05-24 16:38:38 -0400

    fix: aded new key for ec2 instances

--- devops/ec2/keypair.tf
+++ devops/ec2/keypair.tf
@@ -2,7 +2,7 @@ resource "aws_key_pair" "sshkey" {
 
     key_name = "aws-key"
 
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnG+yBBFx6KxeS82Jq6cwJ9sg6SUpisLN6OKvn7T2mKcq6H1l0GvSfrTVikbxgwvOHd3iw5Wa29gA40l+SIH5iIWASpF5nLpxoTJT/8ghff0nFtVjUnXQUwQfef5sxUOzfR0KvoQusos5xArvCC04dlBENJe7i9YFZ0seOR/3x6qSNTv3S9rvxo0RFLRG6IdKN5Rs2wptDGSvfiZ84hylO/u4cEphdjcSLDjfmdrEr0xPcAbvDwBlhnoX9phQxYdGtjb+xzexDEHoCtO7xmnJue+tqAdZPBjygQK4kWKnOIpTV7dGGLAKET+u6iNomw8nIMc39GuOf+C/Q/6Iep7/p tinakamali15gma@tinakamali15gma"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUC2baqqVcg45Csn+L19QViCljNzNV0D5k5aCfHlCw1SC969qV8MBXCHMD4aJ/buKbJ+D173jLYzkxsByrfKL/G+Nl8jtismiGbOQuwvaJOC2QN/2qULV4prFgSvPS6u5pBi8jhs7YJLMmoB5Jwp+TBn9EJxR1BySZZIXgsOdfiX6fNFZWJrqruSfhT6lrqTG7obhXix/N79HxhGrvOchXov50eI42otYGMcGvBAGB2+hcNeYAMpZPKYuJqINX9TnO3HavlT0akJwY2nCPtWPcLkIcFFV8yoIxYXcxIk4Mg9lxKr6R8LQ+RYuSQFKmzNDAvAq1tlS6sLK0FasGXmMd tinakaa@basevm"
 
 }
 
@@ -10,6 +10,6 @@ resource "aws_key_pair" "sshkey2" {
 
    key_name = "aws-key2"
 
-   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDd2wZZPZpZVl4iPaWF+UF2Vrkk2X9mXyMCtil6vBeTbp6V0fHmqYA8ohmi50sgW7+91NR8gOixYKoGG0MCexiO5RAuPAPT2KJ1bexyc2+PCLOHA9NLIqkLR5olYwMCKvNa07uiUZWM7wtC+eOKiOJgbk3VxZR+rjsDvZ+CMiHV9oEJRj17uxUYTU083lFAvucj1enxhxuYmGGNWDkCN0g1i5hUpxdNxl8tW+FaqSBD7m3PupF8mVzLUD2uTt+OomXARH0bU8xKz2gLkFRWYoVs9uCYPHiL97ndqruV52KJccja7c9tnC6PtF/nbK4wkJfSPQtqx9B+vN+ELpL2CIoN tinakamali15gma@tinakamali15gma"
+   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD2qaqcth++qalchByyXrDzSSY9FpvKFvoHnH7udwJiUCPKU3aaQZhzE/1jf4zXEE/+GG4A8lf3R8KRh6WtJ3Br7bHXRLw6OX5T7ByTGKl3FrnPXntvMJcnh+WW8zhlHpSIw7t05mphO36P+6oC7MyUGDU3gg42Udf9u4UNaJN2rvdKPqNnNQh3QPRdcWxSL+kLryF6PwkqI11Pe1SXwb37+rHL3kmcCbVcYnNZ7xMmBS0pA4PkoUkjg/6+NK3SN8GAaJx7fLiLBwwz0pcSz8xz9Rci3FJ8+vzOb+XXqDPyMaQqyeVoP2sah7eT/vUujUe+sqt3eRz6x9IXODxWWllT tinakaa@basevm"
 
 }
