e194063bd774d1ec128390b010e72ff4d6a646b2
Tina Kamali-Anaraki <tina.kamali15@gmail.com>
2022-05-24 14:27:46 +0000

    fix: added region to provider

--- devops/ec2/provider.tf
+++ devops/ec2/provider.tf
@@ -5,6 +5,7 @@ provider "aws" {
 
   token = "FwoGZXIvYXdzEJ///////////wEaDBfe0UrHm0WO+0IJjyK4Aat76JjPDNlUfgTvjlNwEK5O4KkQJSsUOt2JYmfF2zWR/x2GRZz3KLNzgfBqVo6Uy3Ga7ZraGVij9FX+kyGf8Q44IQauqZ/oh75XjiTXGJl9AXKHHi/7d2TAerOW9lAnUgoo2aroDyk4dyMCSGhCcDOxfXQJkkOAjnW7qHP40yvZOW6w4JCfkB2kEZk1mUjK0GTUP+IsOUklWCqHujnrGgaRkn+mO18ZKF395LBhrZNeRjVVuMMYpOAo0b6zlAYyLSjEF0j1dM6u1eTS4u7MXE7Md+NXbClQeyqPanitgvAxe67/r4bc9KQXAtsOdQ=="
 
+  region = var.aws_region
 }
 
 
