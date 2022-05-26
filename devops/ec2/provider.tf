1823ca85c7ada8b43c74a94910282fbd6cab320a
Tina Kamali-Anaraki <tina.kamali15@gmail.com>
2022-05-26 15:49:43 -0400

    fix: using variables for provider

--- devops/ec2/provider.tf
+++ devops/ec2/provider.tf
@@ -1,9 +1,9 @@
 provider "aws" {
 
 #  access_key = "ASIA22VGYN2GZKCXBTVS"
-  access_key = "/home/tinakaa/access"
+  access_key = var.aws_accesskey
 #  secret_key = "OD1QGuXAi5m6DXrsSZ9KKEB4gFuWz4zWfevIp9jR"
-  secret_key = "/home/tinakaa/secret"
+  secret_key = var.aws_secretkey
 
 #  token = "FwoGZXIvYXdzELf//////////wEaDFoZj8j157cV22On/yK4Ac86zKsQYmAs1FFix8rxr3n1usgXQ4qatmDiRPeO/fEIbiF+P0jQoXylKf8TzAYHIWdcj/AvKR5wV8HmkvslkMYhj6Q8WBIGMJDEUfkJJCIR1ZQu8XDH4vyAMSXERWPomz1FKeEVcOfFkbB/aj/f6GLfp42JEIUUuL7S/CL4mFq7LnxlR69UIbAvLee0rP13PL/uJm3joubOZHuvOiHn8vOmOB+lHBpRnLQU8krYT0udOuJ1DZvOnBoojeu4lAYyLd4uwjhrCaWLi4/+3Gi+Z4AH4uI8Fb/TKaLz9S3wqcRO1v0509rQdlYAS2KjFA=="
 
