0eea64e10122b12eb9c64726b54a33d4acf35c41
Tina Kamali-Anaraki <tina.kamali15@gmail.com>
2022-05-26 16:37:10 -0400

    fix: typo in access key and secret key variables

--- devops/ec2/variables.tf
+++ devops/ec2/variables.tf
@@ -38,11 +38,5 @@ variable "availability_zone" {
     default = "us-east-1a"
 }
 
-variable "aws_accesskey" {
-
-    type = string 
-}
-variable "aws_secretkey" {
-
-    type = string
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
