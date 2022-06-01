4c33ef6101e00e0495a99ef1661a74289055f332
tkamali-anaraki <54964035+tkamali-anaraki@users.noreply.github.com>
2022-06-01 09:15:55 -0400

    fix:adding resources-changing instance type. (#4)
    * feat:craeted new branch, added Jenkinsfile to it
    
    * fix: changing aws account, uisng files to put keys.
    
    * fix: terraform plan gave error for using ~
    
    * fix: using quotations
    
    * fix: using variables for provider
    
    * feat: added secret and access varibales
    
    * feat: added aws_accesskey and aws_secretkey values in terraform.tfvars
    
    * fix: adding quotations
    
    * fix: removing quotations
    
    * fix: typo in variable
    
    * fix: typo in access key and secret key variables
    
    * fix:typo in secret key and access key variables
    
    * fix: added file extention
    
    * fix: commented the aws_access_key and aws_secret_key
    
    * feat: adding dockerfile for test
    
    * feat:added line to readme
    
    * fix: adding line to readme to test push for webhook
    
    * fix: adding line to readme to test push for webhook
    
    * fix: adding line to readme to test push for webhook
    
    * fix: trying to test without POL SCM
    
    * fix: examining webhook
    
    * fix: examining webhook
    
    * fix:testing webhook
    
    * fix:testing webhook
    
    * fix:re-added pollscm
    
    * fix:re-added pollscm
    
    * fix:testing webhook
    
    * fix: changing cron
    
    * fix: testing webhook
    
    * fix: updated dockerfile
    
    * fix: modifying docker build
    
    * fix:docker file
    
    * fix: docker build bstart from pwd
    
    * feat:adding user jenkins to group docker
    
    * fix:appending group to user
    
    * fix:typo, groups
    
    * fix:changing folder nsme dockerfile to docker
    
    * feat:removed dockerfile folder
    
    * fix:path in docker build, dockerfile folder to docker
    
    * fix:removing withcredentials from build stage
    
    * feat:added docker login and withcredentials to push stage
    
    * fix:changed credentials for withcredentails in push stage
    
    * feat:adding push and logout comamnd to push stage
    
    * fix:adding a commit
    
    * fix:using t2.medium
    
    * fix:trying the webhook
    
    * feat:modified pull and deploy stages
    
    * fix: changed port
    
    * fix: test, add commit
    
    * fix:change the instance type to give more resources

--- devops/ec2/ec2-2.tf
+++ devops/ec2/ec2-2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2-2" {
 
     ami = "ami-0279c3b3186e54acd"      #for amazon linux ami-04ad2567c9e3d7893
 
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
 
     availability_zone = var.availability_zone
 
