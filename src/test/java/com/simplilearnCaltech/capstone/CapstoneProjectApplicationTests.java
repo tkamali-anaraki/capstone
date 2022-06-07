1596d8281be346ca35faaa2167ab1396305c9923
Tina Kamali-Anaraki <tina.kamali15@gmail.com>
2022-06-06 22:25:53 -0400

    feat: added JUnit test.

--- src/test/java/com/simplilearnCaltech/capstone/CapstoneProjectApplicationTests.java
+++ src/test/java/com/simplilearnCaltech/capstone/CapstoneProjectApplicationTests.java
@@ -2,12 +2,30 @@ package com.simplilearnCaltech.capstone;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CapstoneProjectApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Tina Capstone working.")));
+	}
 }
