1e26951ecd16f3ecb5464a606f5cb0d649b3b767
tkamali-anaraki <54964035+tkamali-anaraki@users.noreply.github.com>
2022-06-08 15:19:01 -0400

    Feat: Removed the container stack after completing the job (#12)
    * feat: added JUnit test.
    
    * feat:added Unit Test stage to CI stages.
    
    * feat:added post to clean up and remove the container after running.
    
    * fix: commenting docker stop in post
    
    * fix: fixing spacing
    
    * fix: fixing spacing for post
    
    * fix: adding post to end of stage
    
    * fix: removed post
    
    * feat:added post again
    
    * feat:changed the docker run command, added name for it, in build stage
    
    * fix:removed force true so key doesnt get regenerated.
    
    * feat: added docker stop to post to remove the container stack after job ran
    
    * fix:modifying syntax
    
    * fix:adding deleteing image comamnd
    
    * fix:syntaxing, post
    
    * fix:removing post commands
    
    * feat:added success-curl stage to see the output of application
    
    * fix: success-curl stage to see the output of application
    
    * feat:added curl to dockerfile
    
    * feat:added curl tag to image to test
    
    * feat:added post comamnd to stop and remove the container
    
    * feat:running container using agent-jenkins image but with curl tag so curl exist in there
    
    * feat: added echo success messages at the end of deply and post, so it shows success after running
    
    * feat: removed echo message after deployment

--- devops/ansible/dockerrun.sh
+++ devops/ansible/dockerrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker run -p 4444:22 -v /var/run/docker.sock:/var/run/docker.sock -d --rm --name=slave1 -e "JENKINS_AGENT_SSH_PUBKEY=$(cat jenkins_agent_key.pub)" tinakamali/agent-jenkins
+sudo docker run -p 4444:22 -v /var/run/docker.sock:/var/run/docker.sock -d --rm --name=slave1 -e "JENKINS_AGENT_SSH_PUBKEY=$(cat jenkins_agent_key.pub)" tinakamali/agent-jenkins:curl
 
